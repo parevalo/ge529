5b8d633652d575b29d6ee2336d572eb8f0d4492d
parevalo <parevalo@bu.edu>
2016-11-17 14:31:25 -0500

    Modify pixel area calculation

--- drought/calc_area.m
+++ drought/calc_area.m
@@ -7,17 +7,18 @@ function [amazon_cellwt] = calc_area(resolution, rows, cols, outname)
     %resolution = 0.05;
     %rows = 3600;
     %cols = 7200;
-
-    R = makerefmat(-180+resolution/2, 90-resolution/2, resolution, -resolution);
+    dir = '/projectnb/landsat/users/parevalo/ge529/ge529/drought/data/';
+    % lat has to be changed too (90 for modis, 50 for trmm)
+    R = makerefmat(-180+resolution/2, 50-resolution/2, resolution, -resolution);
     EP_wt = almanac('earth','wgs84','kilometers');
     [~,wt] = areamat(ones(rows,cols,'single'),R,EP_wt);
     %wt_interest = wt(1601:2200,1);
-    wt_interest = wt(160:279,1);
+    wt_interest = wt(161:280,1);
     amazon_cellwt = repmat(wt_interest,[1 140]);
     %amazon_cellwt = repmat(wt_interest,[1 700]);
 
-    save([outname,'.mat'],'amazon_cellwt');
+    save([dir,outname,'.mat'],'amazon_cellwt');
 end
 
-%cols_rep: from 320 to 460 (140 cols)*
+%cols_rep: from 400 to 540 (140 cols)*
 %rows_rep: from 160 to 280 (120 rows)
