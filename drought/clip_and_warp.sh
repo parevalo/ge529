ed76181dc1abd165bc0c99377151f47018986be7
parevalo <parevalo@bu.edu>
2017-04-18 12:30:53 -0400

    Check if files exist before running the scripts

--- drought/clip_and_warp.sh
+++ drought/clip_and_warp.sh
@@ -8,23 +8,27 @@ cd /projectnb/landsat/users/parevalo/ge529/3B43/TRMM_Full/temp
 
 # TRMM
 for i in $(find . -type f -name "3B43*.HDF"); do
+    
+    # Extract basename
+    fname=$(basename $i | awk -F ".HDF" '{print $1}')
 
     # Assign GCP to TRMM for rotation
-
-    fname=$(basename $i | awk -F ".HDF" '{print $1}')
-    gdal_translate -a_srs EPSG:4326 -gcp 0 0 -180 -50 -gcp 0 1440 180 -50 \
-     -gcp 400 0 -180 50 -gcp 400 1440 180 50 \
-      HDF4_SDS:UNKNOWN:$fname".HDF:0" $fname"_GCP.tif"
+    if [ ! -f $fname"_GCP.tif" ]; then
+        gdal_translate -a_srs EPSG:4326 -gcp 0 0 -180 -50 -gcp 0 1440 180 -50 \
+         -gcp 400 0 -180 50 -gcp 400 1440 180 50 \
+          HDF4_SDS:UNKNOWN:$fname".HDF:0" $fname"_GCP.tif"
+    fi
 
     # Apply GCP to make them permament (i.e rotate)
-
-    gdalwarp -t_srs EPSG:4326 $fname"_GCP.tif" $fname"_warp.tif"
-    
+    if [ ! -f $fname"_warp.tif" ]; then
+        gdalwarp -t_srs EPSG:4326 $fname"_GCP.tif" $fname"_warp.tif"
+    fi 
+        
     # Extract amazon region from TRMM
-
-    gdal_translate -projwin -80 10 -45 -20 -projwin_srs EPSG:4326 \
-     $fname"_warp.tif" $fname"_clip.tif"
-
+    if [ ! -f $fname"_clip.tif" ]; then
+        gdal_translate -projwin -80 10 -45 -20 -projwin_srs EPSG:4326 \
+         $fname"_warp.tif" $fname"_clip.tif"
+    fi
 done
 
 #cd /projectnb/landsat/users/parevalo/ge529/MCD12C1.051 
